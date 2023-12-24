fcba8b23649f38682672dbd834dc13e0a9c4c883
Arif Billah <emran.alarif8@gmail.com>
2023-12-24 06:48:28 +0600

    mongo.html file added for dynamic varipable pussing on the node and display

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
+
 const app = express()
+app.use(cors());
+
 
 function HomeApp(req, res){
     res.send('This is my First HomePage code on Express.JS');
@@ -10,9 +15,29 @@ function about(req, res){
 function contact(req, res){
     res.send('my number is:01779440297<br> My Email is: emran.alarif8@gmail.com');
 }
-
 app.get('/', HomeApp );
 app.get('/about', about );
 app.get('/contact', contact );
-app.listen(3000, () => console.log('Open port 3000'));
+
+app.get('/fol', (req, res) =>{
+    const fruit = {
+        product: 'apple',
+        price: 200
+    }
+    res.send(fruit);
+});
+app.get('/fruits/banana', (req, res) =>{
+    res.send({fruit: 'Banana', quantity: 1000, price: 1000});
+});
+//User templete
+const users = ['arif', 'sayed', 'asad', 'milon', 'hasan']
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+});
+
+
+
+app.listen(4200, () => console.log('Open port 4200'));
 
